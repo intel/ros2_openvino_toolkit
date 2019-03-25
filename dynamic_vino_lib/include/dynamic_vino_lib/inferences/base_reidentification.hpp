80e3db741550ddb43c8609777e2c0f22487c1589
Houk Chen <hongkun.chen@intel.com>
2019-03-25 15:59:16 +0800

    add base_reidentification as base class for person and face reid

---
+++ dynamic_vino_lib/include/dynamic_vino_lib/inferences/base_reidentification.hpp
@@ -0,0 +1,89 @@
+
+
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @brief A header file with declaration for BaseReidentification Class
+ * @file base_reidentification.hpp
+ */
+#ifndef DYNAMIC_VINO_LIB__INFERENCES__BASE_REIDENTIFICATION_HPP_
+#define DYNAMIC_VINO_LIB__INFERENCES__BASE_REIDENTIFICATION_HPP_
+#include <vector>
+#include <cmath>
+#include <unordered_map>
+
+// namespace
+namespace dynamic_vino_lib
+{
+class Tracker
+{
+public:
+  explicit Tracker(int, double, double);
+  /**
+   * @brief Process the new detected track.
+   * @param[in] feature The new detected track feature.
+   * @return The detected track ID.
+   */
+  int processNewTrack(const std::vector<float>& feature);
+
+private:
+  /**
+   * @brief Find the matched track from the recorded tracks.
+   * @param[in] feature The input track's feature.
+   * @return The matched track ID, -1 if it's not matched.
+   */
+  int findMatchTrack(const std::vector<float>& feature);
+  /**
+   * @brief Update the matched track's feature by the new track.
+   * @param[in] track_id The matched track ID.
+   * @param[in] feature The matched track's feature
+   */
+  void updateMatchTrack(int track_id, const std::vector<float>& feature);
+  /**
+   * @brief Update the recorded tracks' lost.
+   */
+  void updateAllTracksLost();
+  /**
+   * @brief Remove the old tracks from the recorded tracks.
+   */
+  void removeOldTracks();
+  /**
+   * @brief Add a new track to the recorded tracks.
+   * @param[in] feature A track's feature.
+   */
+  void addNewTrack(const std::vector<float>& feature);
+  /**
+   * @brief Calculate the cosine similarity between two features.
+   * @return The simlarity result.
+   */
+  double calcSimilarity(
+    const std::vector<float> & feature_a, const std::vector<float> & feature_b);
+
+  struct Track
+  {
+    int lost;
+    std::vector<float> feature;
+  };
+  
+  int lost_track_thresh_ = 10000;
+  int max_track_id_ = -1;
+  double max_match_similarity_ = 0;
+  double same_track_thresh_ = 0.9;
+  double new_track_thresh_ = 0.3;
+  std::unordered_map<int, Track> recorded_tracks_;
+};
+
+}  // namespace dynamic_vino_lib
+#endif  // DYNAMIC_VINO_LIB__INFERENCES__BASE_REIDENTIFICATION_HPP_
--- dynamic_vino_lib/src/inferences/face_reidentification.cpp
+++ dynamic_vino_lib/src/inferences/face_reidentification.cpp
@@ -29,104 +29,10 @@ dynamic_vino_lib::FaceReidentificationResult::FaceReidentificationResult(
   const cv::Rect & location)
 : Result(location) {}
 
-// FaceTracker
-dynamic_vino_lib::FaceTracker::FaceTracker(
-  int drop_lost_thresh, double same_track_thresh, double new_track_thresh)
-: drop_lost_thresh_(drop_lost_thresh),
-  same_track_thresh_(same_track_thresh),
-  new_track_thresh_(new_track_thresh) {}
-
-
-int dynamic_vino_lib::FaceTracker::processNewTrack(const std::vector<float>& feature)
-{
-  int track_id = findMatchTrack(feature);
-  updateAllTracksLost();
-  if (track_id >= 0 and max_match_similarity_ > same_track_thresh_) {
-    updateMatchTrack(track_id, feature);
-  }
-  else if (max_match_similarity_ < new_track_thresh_) {
-    addNewTrack(feature);
-    track_id = max_track_id_;
-  }
-  removeOldTracks();
-  return track_id;
-}
-
-int dynamic_vino_lib::FaceTracker::findMatchTrack(const std::vector<float>& feature)
-{
-  double max_sim = 0;
-  int match_track_id = -1;
-  for (auto iter = recorded_tracks_.begin(); iter != recorded_tracks_.end(); iter++) {
-    double sim = calcSimilarity(feature, iter->second.feature);
-    if (sim > max_sim) {
-      max_sim = sim;
-      match_track_id = iter->first;
-    }
-  }
-  max_match_similarity_ = max_sim;
-  return match_track_id;
-}
-
-double dynamic_vino_lib::FaceTracker::calcSimilarity(
-  const std::vector<float> & feature_a, const std::vector<float> & feature_b) {
-  if (feature_a.size() != feature_b.size()) {
-    throw std::logic_error("cosine similarity can't be called for vectors of different lengths: "
-            "feature_a size = " + std::to_string(feature_a.size()) +
-            "feature_b size = " + std::to_string(feature_b.size()));
-  }
-  float mul_sum, denom_a, denom_b, value_a, value_b;
-  mul_sum = denom_a = denom_b = value_a = value_b = 0;
-  for (auto i = 0; i < feature_a.size(); i++) {
-    value_a = feature_a[i];
-    value_b = feature_b[i];
-    mul_sum += value_a * value_b;
-    denom_a += value_a * value_a;
-    denom_b += value_b * value_b;
-  }
-  if (denom_a == 0 || denom_b == 0) {
-    throw std::logic_error("cosine similarity is not defined whenever one or both "
-            "input vectors are zero-vectors.");
-  }
-  return mul_sum / (sqrt(denom_a) * sqrt(denom_b));
-}
-
-void dynamic_vino_lib::FaceTracker::updateMatchTrack(
-  int track_id, const std::vector<float>& feature) {
-  if (recorded_tracks_.find(track_id) != recorded_tracks_.end()) {
-    recorded_tracks_[track_id].feature.assign(feature.begin(), feature.end());
-    recorded_tracks_[track_id].lost = 0;
-  }
-  else {
-    throw std::logic_error("updating a non-existing track.");
-  }
-}
-
-void dynamic_vino_lib::FaceTracker::updateAllTracksLost() {
-  for (auto iter = recorded_tracks_.begin(); iter != recorded_tracks_.end(); iter++) {
-    iter->second.lost += 1;
-  }
-}
-
-void dynamic_vino_lib::FaceTracker::removeOldTracks() {
-  for (auto iter = recorded_tracks_.begin(); iter != recorded_tracks_.end(); iter++) {
-    if (iter->second.lost > drop_lost_thresh_) {
-      recorded_tracks_.erase(iter);
-    }
-  }
-}
-
-void dynamic_vino_lib::FaceTracker::addNewTrack(const std::vector<float>& feature) {
-  Track track;
-  track.lost = 0;
-  track.feature.assign(feature.begin(), feature.end());
-  max_track_id_ += 1;
-  recorded_tracks_.insert(std::pair<int, Track>(max_track_id_, track));
-}
-
 // FaceReidentification
 dynamic_vino_lib::FaceReidentification::FaceReidentification(double match_thresh)
-: match_thresh_(match_thresh), dynamic_vino_lib::BaseInference() {
-  face_tracker_ = std::make_shared<dynamic_vino_lib::FaceTracker>(1000000, 0.9, 0.3);
+: dynamic_vino_lib::BaseInference() {
+  face_tracker_ = std::make_shared<dynamic_vino_lib::Tracker>(100000, match_thresh, 0.3);
 }
 
 dynamic_vino_lib::FaceReidentification::~FaceReidentification() = default;
@@ -171,62 +77,11 @@ bool dynamic_vino_lib::FaceReidentification::fetchResults()
     std::vector<float> new_face = std::vector<float>(
       output_values + result_length * i, output_values + result_length * (i + 1));
     std::string face_id = "No." + std::to_string(face_tracker_->processNewTrack(new_face));
-    // std::string face_id = findMatchFace(new_face);
     results_[i].face_id_ = face_id;
     found_result = true;
   }
   if (!found_result) {results_.clear();}
   return true;
-}
-
-
-float dynamic_vino_lib::FaceReidentification::calcSimilarity(
-  const std::vector<float> & face_a, const std::vector<float> & face_b)
-{
-  if (face_a.size() != face_b.size()) {
-    throw std::logic_error("cosine similarity can't be called for vectors of different lengths: "
-            "face_a size = " + std::to_string(face_a.size()) +
-            "face_b size = " + std::to_string(face_b.size()));
-  }
-  float mul_sum, denom_a, denom_b, value_a, value_b;
-  mul_sum = denom_a = denom_b = value_a = value_b = 0;
-  for (auto i = 0; i < face_a.size(); i++) {
-    value_a = face_a[i];
-    value_b = face_b[i];
-    mul_sum += value_a * value_b;
-    denom_a += value_a * value_a;
-    denom_b += value_b * value_b;
-  }
-  if (denom_a == 0 || denom_b == 0) {
-    throw std::logic_error("cosine similarity is not defined whenever one or both "
-            "input vectors are zero-vectors.");
-  }
-  return mul_sum / (sqrt(denom_a) * sqrt(denom_b));
-}
-
-std::string dynamic_vino_lib::FaceReidentification::findMatchFace(
-  const std::vector<float> & new_face)
-{
-  auto size = recorded_faces_.size();
-  std::string id = "No.";
-  float best_match_sim = 0;
-  int best_match_ind = -1;
-  for (auto i = 0; i < size; ++i) {
-    float cos_sim = calcSimilarity(new_face, recorded_faces_[i]);
-    if (cos_sim > best_match_sim) {
-      best_match_sim = cos_sim;
-      best_match_ind = i;
-    }
-  }
-  std::cout<<best_match_sim<<std::endl;
-  if (best_match_sim > match_thresh_) {
-    recorded_faces_[best_match_ind] = new_face;
-    return id + std::to_string(best_match_ind);
-  } else {
-    std::cout<<"New Face Detected, probability is:"<<best_match_sim<<std::endl;
-    recorded_faces_.push_back(new_face);
-    return id + std::to_string(size);
-  }
 }
 
 const int dynamic_vino_lib::FaceReidentification::getResultsLength() const
