2e80e79bb517aec6d4aecaf2ddf245d50ff2abf5
RachelRen05 <zhenzhenx.ren@intel.com>
2019-06-27 00:59:55 +0800

    launch.py configuration file reformat

--- sample/launch/pipeline_object_oss.launch.py
+++ sample/launch/pipeline_object_oss.launch.py
@@ -32,7 +32,8 @@ def generate_launch_description():
             package='dynamic_vino_sample', node_executable='pipeline_with_params',
             arguments=['-config', default_yaml],
             remappings=[
-                ('/openvino_toolkit/object/detected_objects', '/ros2_openvino_toolkit/detected_objects'),
+                ('/openvino_toolkit/object/detected_objects',
+                 '/ros2_openvino_toolkit/detected_objects'),
                 ('/openvino_toolkit/object/images', '/ros2_openvino_toolkit/image_rviz')],
             output='screen'),
 
