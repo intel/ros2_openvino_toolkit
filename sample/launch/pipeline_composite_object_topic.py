88dab13208dd43a0b3264f6cdb7edf57eb1f3f63
Lewis Liu <wei.zhi.liu@intel.com>
2019-11-04 17:10:17 +0800

    support rclcpp composable node.
    Signed-off-by: Lewis Liu <wei.zhi.liu@intel.com>

---
+++ sample/launch/pipeline_composite_object_topic.py
@@ -0,0 +1,33 @@
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    default_yaml = os.path.join(get_package_share_directory('dynamic_vino_sample'), 'param',
+                                'pipeline_object_topic.yaml')
+    container = ComposableNodeContainer(
+        node_name='vision_pipeline',
+        node_namespace='',
+        package='rclcpp_components',
+        node_executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='realsense_ros',
+                node_plugin='realsense::RealSenseNodeFactory',
+                node_name='realsense',
+                parameters=[get_package_share_directory('realsense_examples')+'/config/d435i.yaml'],
+                extra_arguments=[{'use_intra_process_comms':'true'}]),
+            ComposableNode(
+                package='dynamic_vino_sample',
+                node_plugin='ComposablePipeline',
+                node_name='composable_pipeline',
+                parameters=[{"config":default_yaml}],
+                #extra_arguments=[{"configg":"default_yaml"}]
+            )
+            ],
+            output='screen',
+    )
+
+    return launch.LaunchDescription([container])
