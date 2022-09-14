ab2ad7d49f1d2eab71440c17fe996fcf4f91b9ad
Fan, Tao <tao.fan@intel.com>
2022-09-14 10:30:02 +0800

    adapt API 2.0 for engines
    Signed-off-by: Fan, Tao <tao.fan@intel.com>

--- dynamic_vino_lib/src/engines/engine_manager.cpp
+++ dynamic_vino_lib/src/engines/engine_manager.cpp
@@ -33,18 +33,18 @@ std::shared_ptr<Engines::Engine> Engines::EngineManager::createEngine(
 #if(defined(USE_OLD_E_PLUGIN_API))
   return createEngine_beforeV2019R2(device, model);
 #else
-  return createEngine_V2019R2_plus(device, model);
+  return createEngine_V2022(device, model);
 #endif
 }
 
-std::shared_ptr<Engines::Engine> Engines::EngineManager::createEngine_V2019R2_plus(
+std::shared_ptr<Engines::Engine> Engines::EngineManager::createEngine_V2022(
   const std::string & device, const std::shared_ptr<Models::BaseModel> & model)
 {
-  InferenceEngine::Core core;
-  auto executable_network = core.LoadNetwork(model->getNetReader(), device);
-  auto request = executable_network.CreateInferRequestPtr();
+  ov::Core core;
+  ov::CompiledModel executable_network = core.compile_model(model->getNetReader(), device);
+  ov::InferRequest infer_request = executable_network.create_infer_request();
 
-  return std::make_shared<Engines::Engine>(request);
+  return std::make_shared<Engines::Engine>(infer_request);
 }
 
 #if(defined(USE_OLD_E_PLUGIN_API))
