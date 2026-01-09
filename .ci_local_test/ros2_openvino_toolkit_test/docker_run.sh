ce240dcddf0230aed0001e29de2e886242c5ab13
jb-balaji <sathiyamoorthi.jb@gmail.com>
2026-01-09 21:52:04 +0100

    Fix CI permission issue: chown test_cases inside container

--- .ci_local_test/ros2_openvino_toolkit_test/docker_run.sh
+++ .ci_local_test/ros2_openvino_toolkit_test/docker_run.sh
@@ -45,7 +45,7 @@ function run_container()
     # Ensure test_cases directory and scripts have proper permissions
     chmod -R 755 "$work_dir"/test_cases
     
-    docker run -i --privileged=true --device=/dev/dri -v "$work_dir"/ros2_openvino_toolkit:/root/${WORKSPACE_DIR}/src/ros2_openvino_toolkit  -v "$HOME"/.Xauthority:/root/.Xauthority -e GDK_SCALE  -v "$work_dir"/test_cases:/root/test_cases --name ros2_openvino_container  ros2_openvino_docker:01 bash -c "cd /root/test_cases && ./run.sh ${ROS_DISTRO}"
+    docker run -i --privileged=true --device=/dev/dri -v "$work_dir"/ros2_openvino_toolkit:/root/${WORKSPACE_DIR}/src/ros2_openvino_toolkit  -v "$HOME"/.Xauthority:/root/.Xauthority -e GDK_SCALE  -v "$work_dir"/test_cases:/root/test_cases --name ros2_openvino_container  ros2_openvino_docker:01 bash -c "chown -R root:root /root/test_cases && cd /root/test_cases && ./run.sh ${ROS_DISTRO}"
 }
 
 if ! run_container; then 
