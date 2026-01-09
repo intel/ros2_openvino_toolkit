8287b6ea9e48d48f91b65a5081bf31472fb9be94
jb-balaji <sathiyamoorthi.jb@gmail.com>
2026-01-09 21:17:30 +0100

    Fix CI test scripts for multi-distro support
    - Update config.sh to detect workspace dir (ros2_ws vs catkin_ws) based on ROS distro
    - Update run.sh to use WORKSPACE_DIR variable instead of hardcoded /root/catkin_ws
    - Add --break-system-packages flag for pip install (Ubuntu 24.04 requirement)
    - Add code_format.yml workflow description comment
    
    Fixes test failures for Jazzy and Humble distros

--- .ci_local_test/ros2_openvino_toolkit_test/test_cases/run.sh
+++ .ci_local_test/ros2_openvino_toolkit_test/test_cases/run.sh
@@ -10,7 +10,7 @@ fi
 #shellcheck source=/dev/null
 source /root/test_cases/config.sh "$ros2_branch"
 
-cd /root/catkin_ws && colcon build --symlink-install
+cd ${WORKSPACE_DIR} && colcon build --symlink-install
 # shellcheck source=/dev/null 
 source ./install/local_setup.bash
 
@@ -18,12 +18,12 @@ apt-get update
 # apt-get install -y ros-$ros2_branch-diagnostic-updater
 apt-get install python3-defusedxml
 apt-get install -y python3-pip
-pip3 install XTestRunner==1.5.0
+pip3 install XTestRunner==1.5.0 --break-system-packages
 
 cd /root/test_cases && ./ros2_openvino_tool_model_download.sh
 mkdir -p /root/test_cases/log
 echo "===cat pipeline_people_ci.yaml"
-cat /root/catkin_ws/install/openvino_node/share/openvino_node/param/pipeline_people_ci.yaml
+cat ${WORKSPACE_DIR}/install/openvino_node/share/openvino_node/param/pipeline_people_ci.yaml
 
 cd /root/test_cases/unittest && python3 run_all.py
 result=$?
